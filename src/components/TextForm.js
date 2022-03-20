052f4cfa13bbbe7c3337fe774512b2cbfc85b587
bhavya <bhavyadhing@gmail.com>
2022-03-21 01:47:56 +0530

    adding logic for uppercase, lowercase, counting words & characters

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -2,20 +2,30 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
 
-  const [text, setText] = useState('Enter Text Here');
+  const [text, setText] = useState('');
   const handleUpClick = ()=>{
-    console.log("clicked button")
+    setText(text.toUpperCase());
+  }  
+  const handleLoClick = ()=>{
+    setText(text.toLowerCase());
   }
   const handleOnChange = (event)=>{
     setText(event.target.value);
   }
   return (
+    <>
     <div>
         <div className="mb-3">
-        <label htmlFor="mybox" className="form-label">{props.heading}</label>
+        <h1>{props.heading}</h1>
         <textarea className="form-control" id="mybox" rows="8" value={text} onChange={handleOnChange} ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick} >Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick} >Convert to UpperCase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick} >Convert to LowerCase</button>
     </div>
+    <div className="container my-3">
+      <h3>Your Text Summary</h3>
+      <p>{text.split(" ").length} words and {text.length} characters.</p>
+    </div>
+    </>
   )
 }
